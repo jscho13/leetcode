33c95ccbb62caad3f2f8cc0139f835b4199f1989
Joseph Cho <jscho13@gmail.com>
2019-01-02 21:29:14 -0500

    add valid number

--- valid_number.rb
+++ valid_number.rb
@@ -5,6 +5,12 @@ def is_number(s)
   s = s.lstrip.rstrip.downcase
   # false if it has any characters except for e, numbers, +, or -
   return false if /[^e0-9+-\.]/.match(s)
+  # false if it has no numbers
+  return false if /[0-9]/.match(s).nil?
+  # false if +/- is after the .
+  return false if /(?<=[0-9\.])[+-]/.match(s) != nil
+  # fail if two .
+  return false if s.count('.') > 1
   # scientific notation
   if s.include?('e')
     # false if more then one e
@@ -20,13 +26,16 @@ def is_number(s)
     # fail if two +/-
     return false if s[0].count('-') + s[0].count('+') > 1
     return false if s[1].count('-') + s[1].count('+') > 1
-  # normal notation
+    # fail if +/- isn't in start
+    return false if (/(?<=[0-9])[+-]/.match(s[0]) || /(?<=[0-9])[+-]/.match(s[1])) != nil
+    # fail if no number before/after  e
+    return false if (/[0-9]/.match(s[0]) && /[0-9]/.match(s[1])).nil?
+  # rormal notation
   else
+    # fail if two +/-
     return false if s.count('-') + s.count('+') > 1
-    # it's okay for 3.
+  # normal notation
   end
   # true if it passes all these things
   return true
 end
-
-puts is_number('.')
