c0a659cdf8a5ebf566117ac5e0ee7999d79014c6
Joseph Cho <jscho13@gmail.com>
2019-01-07 22:48:29 -0500

    progress on shortest palindrome

--- shortest_palindrome.rb
+++ shortest_palindrome.rb
@@ -3,27 +3,21 @@
 def shortest_palindrome(s)
   max = s.length - 1
   pointer = s.length
-  s = s.reverse.split('')
-  i = s.length/2
-  ans = s.clone
-  i.times do |idx|
-    if s[idx] != s[pointer-1]
-      ans.insert(pointer, s[idx])
-    else
-      pointer -= 1 
+  s_ary = s.split('')
+  parsed_chars = []
+  l = 0
+  s_ary.each_with_index do |x, idx|
+    parsed_chars << x
+    palin_finder = parsed_chars.join('')
+    if palin_finder == palin_finder.reverse && palin_finder.length != 1
+      l = palin_finder.length 
     end
   end
 
-	# if this isn't a palindrome, do inside out?
-  ans.reverse.join('')
+  ans = if l == 0
+          (parsed_chars.drop(1).reverse + s_ary).join('')
+        else
+          (parsed_chars.drop(l).reverse + s_ary).join('')
+        end
+  ans
 end
-
-puts shortest_palindrome('ab') # bab
-puts shortest_palindrome('bab') # bab
-puts shortest_palindrome('abab') # babab
-
-puts shortest_palindrome('abbaa') # aabbaa
-puts shortest_palindrome('aabba') # abbaabba
-
-puts shortest_palindrome('aabbaaa') # aaabbaaa
-puts shortest_palindrome('aaabbaa') # aabbaaabbaa
