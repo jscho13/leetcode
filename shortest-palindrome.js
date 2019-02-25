79b2505d5b8a8b0772a0147ac7a408daffd35711
Joseph Cho <jscho13@gmail.com>
2019-02-24 22:06:40 -0500

    Rename and do shortest palindrome in js

---
+++ shortest-palindrome.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var shortestPalindrome = function(s) {
+  var pointer1 = 0;
+  var pointer2 = s.length;
+
+  for (var c=s.length; c<s.length; c++) {
+    
+  }
+};
