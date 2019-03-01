521aebd1d3a257c64291b85db7dcbdcec8177145
Joseph Cho <jscho13@gmail.com>
2019-03-01 17:20:01 -0500

    Solve shortest-palindrome

--- shortest-palindrome.js
+++ shortest-palindrome.js
@@ -3,10 +3,29 @@
  * @return {string}
  */
 var shortestPalindrome = function(s) {
-  var pointer1 = 0;
-  var pointer2 = s.length;
+  var shift = 0;
+  var l = s.length;
+  var reverse = s.split('').reverse().join('');
 
-  for (var c=s.length; c<s.length; c++) {
-    
+  // check here if it's already a plaindrome
+  var palindrome = true;
+  for (var i=0; i<l/2; i++) {
+    if (s[i] !== s[l-i-1]) {
+      palindrome = false;
+    }
   }
+  if (palindrome) return s;
+
+  // The trick is to start with the reverse (since that by nature is the worst case)
+  // And shift the original string once to the right to act like we've appended a character
+  // Once you get a match, you know that section is a palindrome and the rest need to be appended
+  for (var i=1; i < s.length; i++) {
+    if (s.slice(0, l-i) === reverse.slice(i, l)) {
+      shift = i;
+      break;
+    }
+  }
+
+  var remainder = reverse.slice(0, shift);
+  return remainder + s;
 };
