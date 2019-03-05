6d9610b1815096d236d1c8e093eb39378c210e1b
Joseph Cho <jscho13@gmail.com>
2019-03-05 15:56:55 -0500

    LRU-cache solution

--- lru-cache.js
+++ lru-cache.js
@@ -1,3 +1,7 @@
+// To achieve O(1) time-complexity you can't use indexOf
+// Need to implement a double linked list
+// Some idea of nodes with a prev, next, and a beg/end to push and pop from the linked list
+
 /**
  * @param {number} capacity
  */
@@ -7,7 +11,7 @@ var LRUCache = function(capacity) {
   this.capacity = capacity;
 };
 
-/** 
+/**
  * @param {number} key
  * @return {number}
  */
@@ -15,17 +19,17 @@ LRUCache.prototype.get = function(key) {
   if (this.cache[key] === undefined) {
     return -1;
   }
-  
+
   var q = this.lruQueue;
   var idx = q.indexOf(key);
-  q.splice(idx, idx+1);
+  q.splice(idx, 1);
   q.push(key);
-  
+
   return this.cache[key];
 };
 
-/** 
- * @param {number} key 
+/**
+ * @param {number} key
  * @param {number} value
  * @return {void}
  */
@@ -33,28 +37,19 @@ LRUCache.prototype.put = function(key, value) {
   this.cache[key] = value;
 
   var q = this.lruQueue;
+
   var idx = q.indexOf(key);
   if (idx !== -1) {
-    q.splice(idx, idx+1);
+    q.splice(idx, 1);
     q.push(key);
   } else {
     q.push(key);
   }
-  
+
   var deleteKey;
   if (q.length > this.capacity) {
     deleteKey = q.shift();
   }
-  
+
   delete this.cache[deleteKey];
 };
-
-
-var obj = new LRUCache(2);
-console.log(obj.put(2,1))
-console.log(obj.put(1,1))
-console.log(obj.put(2,3))
-console.log(obj.put(4,1))
-console.log(obj.get(1))
-console.log(obj.get(2))
-
