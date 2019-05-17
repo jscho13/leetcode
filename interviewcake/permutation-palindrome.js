21744ba0ffcdc200676441dd7468f9cb4c132ee2
Joseph Cho <jscho13@gmail.com>
2019-05-17 15:21:25 -0400

    Add permutation palindrome

---
+++ interviewcake/permutation-palindrome.js
@@ -0,0 +1,11 @@
+function permutations(string) { 
+}
+
+
+console.log(permutation('civic'));
+console.log(permutation('ivicc'));
+console.log(permutation('ll'));
+console.log(permutation('l'));
+console.log(permutation(''));
+console.log(permutation('civil'));
+console.log(permutation('livci'));
