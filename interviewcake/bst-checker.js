9a1e6a346bf28222b377368bf0d86c089f98ee8d
Joseph Cho <jscho13@gmail.com>
2019-06-16 23:29:30 -0400

    Complete bst checker

---
+++ interviewcake/bst-checker.js
@@ -0,0 +1,87 @@
+class BinaryTreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left  = null;
+    this.right = null;
+  }
+
+  insertLeft(value) {
+    this.left = new BinaryTreeNode(value);
+    return this.left;
+  }
+
+  insertRight(value) {
+    this.right = new BinaryTreeNode(value);
+    return this.right;
+  }
+}
+
+
+function isBinarySearchTree(treeRoot) {
+  let left, right;
+  let n = treeRoot;
+  if (n === null) return true;
+  if (checkAll(n.left, n.val, 'l') === false) return false;
+  if (checkAll(n.right, n.val, 'r') === false) return false;
+
+  return isBinarySearchTree(n.left) && isBinarySearchTree(n.right);
+}
+
+function checkAll(root, val, dir) {
+  let n = root;
+  if (n === null) return true;
+  if (n.val > val && dir === 'l') return false;
+  if (n.val < val && dir === 'r') return false;
+
+  return checkAll(n.left, val, dir) && checkAll(n.right, val, dir);
+}
+
+
+// Tests
+
+let desc = 'valid full tree';
+let treeRoot = new BinaryTreeNode(50);
+let leftNode = treeRoot.insertLeft(30);
+leftNode.insertLeft(10);
+leftNode.insertRight(40);
+let rightNode = treeRoot.insertRight(70);
+rightNode.insertLeft(60);
+rightNode.insertRight(80);
+assertEquals(isBinarySearchTree(treeRoot), true, desc);
+
+desc = 'both subtrees valid';
+treeRoot = new BinaryTreeNode(50);
+leftNode = treeRoot.insertLeft(30);
+leftNode.insertLeft(20);
+leftNode.insertRight(60);
+rightNode = treeRoot.insertRight(80);
+rightNode.insertLeft(70);
+rightNode.insertRight(90);
+assertEquals(isBinarySearchTree(treeRoot), false, desc);
+
+desc = 'descending linked list';
+treeRoot = new BinaryTreeNode(50);
+leftNode = treeRoot.insertLeft(40);
+leftNode = leftNode.insertLeft(30);
+leftNode = leftNode.insertLeft(20);
+leftNode = leftNode.insertLeft(10);
+assertEquals(isBinarySearchTree(treeRoot), true, desc);
+
+desc = 'out of order linked list';
+treeRoot = new BinaryTreeNode(50);
+rightNode = treeRoot.insertRight(70);
+rightNode = rightNode.insertRight(60);
+rightNode = rightNode.insertRight(80);
+assertEquals(isBinarySearchTree(treeRoot), false, desc);
+
+desc = 'one node tree';
+treeRoot = new BinaryTreeNode(50);
+assertEquals(isBinarySearchTree(treeRoot), true, desc);
+
+function assertEquals(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`)
+  }
+}
