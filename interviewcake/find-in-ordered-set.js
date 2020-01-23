5f5cecb7e479d3d1883e60bc55969c8deb1ac1ad
Joseph Cho <jscho13@gmail.com>
2020-01-22 21:38:26 -0500

    Find in ordered set

---
+++ interviewcake/find-in-ordered-set.js
@@ -0,0 +1,91 @@
+
+// O(log N) solution
+
+function contains(array, value) {
+  // Check if an integer is present in the array
+  while (array.length > 0) {
+    let mid = Math.floor(array.length/2);
+    if (array[mid] < value) {
+      array = array.splice(mid+1, array.length);
+    } else if (array[mid] > value) {
+      array = array.splice(0, mid);
+    } else {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Tests
+
+let desc = 'empty array';
+let actual = contains([], 1);
+let expected = false;
+assertEquals(actual, expected, desc);
+
+desc = 'one item array, number present';
+actual = contains([1], 1);
+expected = true;
+assertEquals(actual, expected, desc);
+
+desc = 'one item array, number absent';
+actual = contains([1], 2);
+expected = false;
+assertEquals(actual, expected, desc);
+
+desc = 'small array, number present';
+actual = contains([2, 4, 6], 4);
+expected = true;
+assertEquals(actual, expected, desc);
+
+desc = 'small array, number absent';
+actual = contains([2, 4, 6], 5);
+expected = false;
+assertEquals(actual, expected, desc);
+
+desc = 'large array, number present';
+actual = contains([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8);
+expected = true;
+assertEquals(actual, expected, desc);
+
+desc = 'large array, number absent';
+actual = contains([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0);
+expected = false;
+assertEquals(actual, expected, desc);
+
+desc = 'large array, number first';
+actual = contains([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1);
+expected = true;
+assertEquals(actual, expected, desc);
+
+desc = 'large array, number last';
+actual = contains([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10);
+expected = true;
+assertEquals(actual, expected, desc);
+
+function assertEquals(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`);
+  }
+}
---
+++ interviewcake/which-appears-twice.js
@@ -0,0 +1,62 @@
+function findRepeat(numbers) {
+  // Find the number that appears twice
+  let reducer = (acc, curr) => acc+curr;
+	let sum = numbers.reduce(reducer, 0);
+
+	let n = numbers.length-1;
+	let expectedSum = (n+1) * n/2; 
+  
+
+  return sum-expectedSum;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Tests
+
+let desc = 'short array';
+let actual = findRepeat([1, 2, 1]);
+let expected = 1;
+assertEqual(actual, expected, desc);
+
+desc = 'medium array';
+actual = findRepeat([4, 1, 3, 4, 2]);
+expected = 4;
+assertEqual(actual, expected, desc);
+
+desc = 'long array';
+actual = findRepeat([1, 5, 9, 7, 2, 6, 3, 8, 2, 4]);
+expected = 2;
+assertEqual(actual, expected, desc);
+
+function assertEqual(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`)
+  }
+}
+
+function assertThrowsError(func, desc) {
+  try {
+    func();
+    console.log(`${desc} ... FAIL`);
+  } catch (e) {
+    console.log(`${desc} ... PASS`);
+  }
+}
