8e51e3bb1147a5a4659ca6375195d47654a08d78
Joseph Cho <jscho13@gmail.com>
2020-02-25 20:52:05 -0500

    greedy algorithims

--- best-time-to-buy-and-sell-stock.js
+++ best-time-to-buy-and-sell-stock.js
@@ -1,20 +1,111 @@
 
-var maxProfit = function(prices) {
-  var price = 0;
-  var diff = 0;
-  var max = Number.MIN_SAFE_INTEGER;
-  var min = Infinity;
-  for (var p=0; p<prices.length; p++) {
-    if (prices[p] < min) {
-      min = prices[p];
-      max = prices[p];
-    } else if (prices[p] > max) {
-      max = prices[p];
-      diff = max-min;
-    }
-    
-    price = Math.max(diff, price);
-  }
-  
-  return price;
-};
+/**
+* @param {number[]} prices
+* @return {number}
+*/
+
+// You have the answer down there. DON'T LOOK. Can you do better?
+const maxProfit = (prices) => {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 2019
+// const maxProfit = (prices) => {
+//   if (prices.length < 2) return 0;
+//   let b = prices[0];
+//   let s = prices[1];
+//   let diff = s-b;
+//   let lowest = Math.min(b,s);
+//   for (let i=2; i<prices.length; i++) {
+//     let diff2 = prices[i]-lowest;
+//     if (diff2>diff) {
+//       b=lowest;
+//       s=prices[i];
+//       diff = diff2;
+//     }
+//     if (prices[i]<lowest) lowest = prices[i];
+//   }
+// 
+//   return (s-b < 0) ? 0 : s-b;
+// };
+
+// 2018
+// var maxProfit = function(prices) {
+//   var price = 0;
+//   var diff = 0;
+//   var max = Number.MIN_SAFE_INTEGER;
+//   var min = Infinity;
+//   for (var p=0; p<prices.length; p++) {
+//     if (prices[p] < min) {
+//       min = prices[p];
+//       max = prices[p];
+//     } else if (prices[p] > max) {
+//       max = prices[p];
+//       diff = max-min;
+//     }
+//     
+//     price = Math.max(diff, price);
+//   }
+//   
+//   return price;
+// };
+
+// Interview cake
+// function getMaxProfit(prices) {
+//   if (prices.length < 2) return 0;
+//   let peak = prices[1]-prices[0];
+//   let total = peak;
+//
+//   for (let i=2; i<prices.length; i++) {
+//     let diff = prices[i]-prices[i-1];
+//     total = diff+total;
+//     if (diff > total) total = diff;
+//     if (total > peak) peak = total;
+//   }
+//
+//   return peak;
+// }
--- interviewcake/stock-price.js
+++ interviewcake/stock-price.js
@@ -2,20 +2,42 @@
 // Time-complexity: O(n)
 // Space-complexity: O(1)
 
-function getMaxProfit(prices) {
-  if (prices.length < 2) return 0;
-  let peak = prices[1]-prices[0];
-  let total = peak;
-
+const getMaxProfit = (prices) => {
+  if (prices.length < 2) throw new Error();
+  let b = prices[0];
+  let s = prices[1];
+  let diff = s-b;
+  let lowest = Math.min(b,s);
   for (let i=2; i<prices.length; i++) {
-    let diff = prices[i]-prices[i-1];
-    total = diff+total;
-    if (diff > total) total = diff;
-    if (total > peak) peak = total;
+    let diff2 = prices[i]-lowest;
+    if (diff2>diff) {
+      b=lowest;
+      s=prices[i];
+      diff = diff2;
+    }
+    if (prices[i]<lowest) lowest = prices[i];
   }
 
-  return peak;
-}
+  return s-b;
+};
+
+
+// function getMaxProfit(prices) {
+//   if (prices.length < 2) return 0;
+//   let peak = prices[1]-prices[0];
+//   let total = peak;
+// 
+//   for (let i=2; i<prices.length; i++) {
+//     let diff = prices[i]-prices[i-1];
+//     total = diff+total;
+//     if (diff > total) total = diff;
+//     if (total > peak) peak = total;
+//   }
+// 
+//   return peak;
+// }
+
+
 
 
 // Tests
