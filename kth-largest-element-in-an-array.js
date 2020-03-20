26d6f7b9ccd58c028ab8d3c452624305c3c2d217
Joseph Cho <jscho13@gmail.com>
2020-03-20 00:27:15 -0400

    first quicksort

--- kth-largest-element-in-an-array.js
+++ kth-largest-element-in-an-array.js
@@ -3,35 +3,29 @@
  * @param {number} k
  * @return {number}
  */
+ 
+// Supposedly O(n) and O(n^2) worst case
+const findKthLargest = (nums, k) => {
+  const quickSort = (l, r) => {
+    if (l >= r) return;
+    
+    let searchL = l;
+    let searchR = r;
+    let pivot = r;
 
-function findKthLargest(nums, k) {
-  nums = mergeSort(nums)
-
-  for(var i=0; i<nums.length; i++) {
-    if (k == i) break;
-    ans = nums[i];
+    while (searchL < searchR) {
+      while (nums[searchL]<nums[pivot])  searchL++;
+      while (nums[searchR]>=nums[pivot]) searchR--;
+      if (searchL < searchR) {
+        [nums[searchL], nums[searchR]] = [nums[searchR], nums[searchL]];
+      }
+    }
+    
+    [nums[pivot], nums[searchL]] = [nums[searchL], nums[pivot]];
+    quickSort(l, searchL-1);
+    quickSort(searchL+1, r);
   }
-  return ans;
-}
-
-function mergeSort(arry) {
-  if (arry.length == 1 || arry.length ==0) return arry;
-  var mid = Math.floor(arry.length/2);
-  var left = arry.slice(0, mid);
-  var right = arry.slice(mid, arry.length)
-  return merge(mergeSort(left), mergeSort(right));
-}
-
-function merge(left, right) {
-  var merged = [];
-  var lidx = 0;
-  var ridx = 0;
-  while (left.length > 0 && right.length > 0) {
-    left[0] >= right[0] ? merged.push(left.shift()) : merged.push(right.shift());
-  }
-
-  if (left.length > 0) merged = merged.concat(left);
-  if (right.length > 0) merged = merged.concat(right);
-  return merged;
-}
-
+  
+  quickSort(0, nums.length-1);
+  return nums[nums.length-k];
+};
