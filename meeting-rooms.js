ec9c56a4f91017d755e1806d3a685726cc8a7e08
Joseph Cho <jscho13@gmail.com>
2020-11-01 16:41:11 -0500

    ideal attempt

--- meeting-rooms.js
+++ meeting-rooms.js
@@ -1,25 +1,20 @@
 /**
  * @param {number[][]} intervals
  * @return {boolean}
- */
-// O(nlogn+n)
+*/
+// Attempt 2
+// O(nlogn)
 // S(logn)
-// theres the simpler way... but here you sorted by ending cause meeting-rooms-ii scarred you
 const canAttendMeetings = (intervals) => {
-  if (intervals.length === 0) return true;
-  
-  let intv = intervals.sort((a,b) => {
-    if (a[1] === b[1]) return a[0]-b[0];
-    return a[1]-b[1];
-  });
-  
-  let meeting = intv.pop();
-  let start = meeting[0];
-  while (intv.length) {
-    meeting = intv.pop();
-    if (meeting[1] > start) return false;
-    start = meeting[0];
-  }
-
-  return true;
+    let sorted = intervals.sort((a, b) => {
+        if (a[0]===b[0]) return a[1]-b[1];
+        return a[0]-b[0];
+    })
+    
+    while (sorted.length > 1) {
+        let int = sorted.shift();
+        if (int[1] > sorted[0][0]) return false;
+    }
+    
+    return true;
 };
