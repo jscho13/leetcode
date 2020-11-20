6fa053599263b434e7f0bdb273d268bf074a0d57
Joseph Cho <jscho13@gmail.com>
2020-11-20 01:50:29 -0500

    decode the string

--- decode-string.js
+++ decode-string.js
@@ -1,6 +1,49 @@
+// Attempts: 4
+/**
+ * @param {string} s
+ * @return {string}
+ */
+// O(n)
+// S(n)
+const decodeString = (s) => {
+    let q = [];
+
+    for (let i=0; i<s.length; i++) {
+        let ch = s[i];
+        if (ch === ']') {
+            let substr = '';
+            while (q[q.length-1] != '[') {
+                let item = q.pop();
+                substr = `${item}${substr}`;
+            }
+            q.pop(); // gets rid of trailing [
+
+            // calculate count
+            let count = '';
+            while (!isNaN(q[q.length-1])) {
+                let num = q.pop();
+                count = `${num}${count}`;
+            }
+            
+            let str = '';
+            let parsed = parseInt(count);
+            while (count>0) {
+                str += substr;
+                count--;
+            }
+            q.push(str);
+        } else {
+            q.push(ch);
+        }
+    }
+    
+    return q.join('');
+};
+
+
+
 
 // Attempts: 3
-
 function decodeString(s) {
   var stack = [];
   for (var i=0; i<s.length; i++) {
@@ -35,4 +78,6 @@ function decodeString(s) {
   var ans = stack.join('');
   return ans;
 }
+
+
 
