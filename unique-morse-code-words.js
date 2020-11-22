96c3d4f24add9c777b94acd75c26cdbba703ddfe
Joseph Cho <jscho13@gmail.com>
2020-11-22 18:41:32 -0500

    unique morse code

--- unique-morse-code-words.js
+++ unique-morse-code-words.js
@@ -2,6 +2,22 @@
  * @param {string[]} words
  * @return {number}
  */
+// Attempt 2
+const uniqueMorseRepresentations = (words) => {
+    let morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
+    let map = {};
+    for (let i=0; i<words.length; i++) {
+        let word = words[i];
+        let str = '';
+        for (let j=0; j<word.length; j++) {
+            str += morse[word.charCodeAt(j)-97];
+        }
+        map[str] = map[str]+1 || 1;
+    }    
+    return Object.keys(map).length;
+};
+
+// Attempt 1
 const uniqueMorseRepresentations = (words) => {
   const dict = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
   const trans = words.map(word => {
@@ -10,15 +26,3 @@ const uniqueMorseRepresentations = (words) => {
   
   return [...new Set(trans)].length;
 };
-
-
-// A very sexy solution:
-// var uniqueMorseRepresentations = (words) => {
-//   const morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
-//     
-//   const mapper = char => morse[char.charCodeAt(0) - 'a'.charCodeAt(0)];
-//   
-//   return words.map((word)=>{return word.split('').map(mapper).join('')})
-//     .reduce((set, cur)=>set.add(cur), new Set()).size  
-//   
-// }
