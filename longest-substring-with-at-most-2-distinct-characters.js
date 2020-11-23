17daa9614a9a2a3834b7955c254bae6f77e58118
Joseph Cho <jscho13@gmail.com>
2020-11-23 01:18:53 -0500

    longest substring

---
+++ longest-substring-with-at-most-2-distinct-characters.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+// O(n)
+// S(n)
+const lengthOfLongestSubstringTwoDistinct = (s) => {
+    let q={};
+    let l=0;
+    let r=0;
+    let ans=0;
+    let total=0;
+    
+    while (r < s.length) {
+        let cr = s[r];
+        q[cr] = q[cr]+1 || 1;
+        if (q[cr] === 1) {
+            total++;
+            
+            while (total > 2) {
+                let cl = s[l];
+                q[cl]--;
+                if (q[cl] === 0) total--;
+                l++;
+            }
+        }
+        r++;
+        ans = Math.max(ans, r-l);
+    }
+    
+    return ans;
+};
