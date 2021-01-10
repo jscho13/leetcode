900d8868548da1244e0b49e9528752ca313e9563
Joseph Cho <jscho13@gmail.com>
2021-01-10 00:29:38 -0500

    tabulation

--- partition-equal-subset-sum.js
+++ partition-equal-subset-sum.js
@@ -32,3 +32,39 @@ const canPartition = (nums) => {
 const sum = (ary) => {
     return ary.reduce((x, acc) => { return x+acc }, 0);
 }
+
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+// Tabulation
+// O(n*sum)
+// S(n*sum)
+const canPartition = (nums) => {
+    let sum = nums.reduce((x, acc) => { return x+acc }, 0);
+    if (sum%2 !== 0) return false;
+    let half = sum/2;
+    
+    let dp = new Array(nums.length);
+    for (let i=0; i<dp.length; i++) {
+        dp[i] = new Array(half+1);
+        dp[i][0] = true;
+    }
+    
+    for (let r=0; r<nums.length; r++) {
+        for (let s=1; s<=half; s++) {
+            let c = nums[r];
+            if ((s-c)===0) {
+                dp[r][s] = true;
+            } else if (dp[r-1] && dp[r-1][s] === true) {
+                dp[r][s] = true;
+            } else if (dp[r-1] && dp[r-1][s-c] === true) {
+                dp[r][s] = true;
+            }
+        }
+        if (dp[r][half]) return true;
+    }
+
+    return false;
+};
